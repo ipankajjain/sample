fca16218d55bdbdb19494363207de022b4d807ae
ipankajjain <jain.pankaj84@gmail.com>
2017-02-03 18:44:29 +0530

    project 1

---
+++ project1/src/main/java/com/dev/sample/project1/App.java
@@ -0,0 +1,13 @@
+package com.dev.sample.project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
